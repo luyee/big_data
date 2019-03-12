bc71ad8ce9fae7ad78da99869b522e752bc18765
caiwei <caiwei@dsgdata.com>
2019-03-12 10:21:44 +0800

    feat(CreateData): 生成测试数据

--- java/src/main/java/com/caiw/generatedata/CreateData.java
+++ java/src/main/java/com/caiw/generatedata/CreateData.java
@@ -11,7 +11,7 @@ import java.util.*;
 /**
  * 模拟数据程序
  *
- * @author Administrator
+ * @author caiw
  */
 
 public class CreateData {
@@ -22,11 +22,11 @@ public class CreateData {
      * @param sc
      * @param sqlContext
      */
-    public static final String ACTIONPATH = "D:\\test\\TXT\\action.txt";
-    public static final String USERPATH = "D:\\test\\TXT\\user.txt";
-    public static final String PRODUCTPATH = "D:\\test\\TXT\\product.txt";
+    private static final String ACTION_PATH = "D:\\test\\TXT\\action.txt";
+    private static final String USER_PATH = "D:\\test\\TXT\\user.txt";
+    private static final String PRODUCT_PATH = "D:\\test\\TXT\\product.txt";
 
-    public static void mock() {
+    private static void mock() {
 
         String[] searchKeywords = new String[]{"火锅", "蛋糕", "重庆辣子鸡", "重庆小面",
                 "呷哺呷哺", "新辣道鱼火锅", "国贸大厦", "太古商场", "日本料理", "温泉"};
@@ -35,12 +35,12 @@ public class CreateData {
         String date = format.format(new Date(System.currentTimeMillis())).substring(0,10);
         String[] actions = new String[]{"search", "click", "order", "pay"};
         Random random = new Random();
-        CreateData.createFile(ACTIONPATH);
+        CreateData.createFile(ACTION_PATH);
         for (int i = 0; i < 50; i++) {
-            long userid = random.nextInt(100);
+            long userId = random.nextInt(100);
 
             for (int j = 0; j < 10; j++) {
-                String sessionid = UUID.randomUUID().toString().replace("-", "");
+                String sessionId = UUID.randomUUID().toString().replace("-", "");
                 String baseActionTime = date + " " + random.nextInt(23);
 
                 long clickCategoryId = 1;
@@ -56,29 +56,34 @@ public class CreateData {
                     String payProductIds = null;
 
                     String action = actions[random.nextInt(4)];
-                    if ("search".equals(action)) {
-                        searchKeyword = searchKeywords[random.nextInt(10)];
-                    } else if ("click".equals(action)) {
-                        if (clickCategoryId == 1) {
-                            clickCategoryId = Long.valueOf(String.valueOf(random.nextInt(100)))+1;
-                        }
-                        clickProductId = Long.valueOf(String.valueOf(random.nextInt(9)))+1;
+                    switch (action) {
+                        case "search":
+                            searchKeyword = searchKeywords[random.nextInt(10)];
+                            break;
+                        case "click":
+                            if (clickCategoryId == 1) {
+                                clickCategoryId = Long.valueOf(String.valueOf(random.nextInt(100))) + 1;
+                            }
+                            clickProductId = Long.valueOf(String.valueOf(random.nextInt(9))) + 1;
 
-                    } else if ("order".equals(action)) {
-                        orderCategoryIds = String.valueOf(random.nextInt(100));
-                        orderProductIds = String.valueOf(random.nextInt(100));
-                    } else if ("pay".equals(action)) {
-                        payCategoryIds = String.valueOf(random.nextInt(100));
-                        payProductIds = String.valueOf(random.nextInt(100));
+                            break;
+                        case "order":
+                            orderCategoryIds = String.valueOf(random.nextInt(100));
+                            orderProductIds = String.valueOf(random.nextInt(100));
+                            break;
+                        case "pay":
+                            payCategoryIds = String.valueOf(random.nextInt(100));
+                            payProductIds = String.valueOf(random.nextInt(100));
+                            break;
                     }
 
                     int city_id = random.nextInt(31)+1;
-                    String wordLine = date + "," + userid + "," + sessionid + "," + pageId + ","
+                    String wordLine = date + "," + userId + "," + sessionId + "," + pageId + ","
                             + actionTime + "," + searchKeyword + "," +clickCategoryId+","
                             + clickProductId + "," + orderCategoryIds + "," + orderProductIds
                             + "," + payCategoryIds + "," + payProductIds+","+city_id;
 
-                    CreateData.upData(ACTIONPATH, wordLine);
+                    CreateData.upData(ACTION_PATH, wordLine);
                 }
             }
         }
@@ -88,18 +93,18 @@ public class CreateData {
          * ========================用户==========================================
          */
 
-        CreateData.createFile(USERPATH);
+        CreateData.createFile(USER_PATH);
         String[] sexes = new String[]{"male", "female"};
         for (int i = 0; i < 100; i++) {
-            long userid = i;
+            String userId = String.valueOf(i);
             String username = "user" + i;
             String name = "name" + i;
             int age = random.nextInt(60);
             String professional = "professional" + random.nextInt(100);
             String city = "city" + random.nextInt(100);
             String sex = sexes[random.nextInt(2)];
-            String wordLine = userid + "," + username + "," + name + "," + age + "," + professional + "," + city + "," + sex;
-            CreateData.upData(USERPATH, wordLine);
+            String wordLine = userId + "," + username + "," + name + "," + age + "," + professional + "," + city + "," + sex;
+            CreateData.upData(USER_PATH, wordLine);
 
         }
 
@@ -108,39 +113,45 @@ public class CreateData {
          * ===================产品===============================================
          */
 
-        CreateData.createFile(PRODUCTPATH);
+        CreateData.createFile(PRODUCT_PATH);
         int[] productStatus = new int[]{0, 1};
 
         for (int i = 0; i < 100; i++) {
-            long productId = i;
+            String productId = String.valueOf(i);
             String productName = "product" + i;
             String extendInfo = "" + productStatus[random.nextInt(2)] ;
             String worldLine = productId+","+productName+","+extendInfo;
-            CreateData.upData(PRODUCTPATH,worldLine);
+            CreateData.upData(PRODUCT_PATH,worldLine);
         }
     }
 
 
-    public static void createFile(String path) {
+    private static void createFile(String path) {
         File file = new File(path);
         if (file.exists()) {
-            file.delete();
+            boolean delete = file.delete();
+            if(delete){
+                System.out.println("delete success");
+            }
         } else {
             try {
-                file.createNewFile();
+                boolean newFile = file.createNewFile();
+                if(newFile){
+                    System.out.println("create success");
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public static void upData(String path, String data) {
+    private static void upData(String path, String data) {
         try {
-            FileWriter fileWritter = new FileWriter(path, true);
-            BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
-            bufferWritter.write(data + "\r\n");
-            bufferWritter.flush();
-            bufferWritter.close();
+            FileWriter fileWriter = new FileWriter(path, true);
+            BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+            bufferWriter.write(data + "\r\n");
+            bufferWriter.flush();
+            bufferWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
